c42a45fb68061b2e1daee075e5bad6ab2676adfa
Brayan Aguilar <brayan.aguilar@teia.mx>
2026-02-25 02:03:03 -0600

    Deleted pipeline.yml test.yml Updated main.ts

--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -2,10 +2,10 @@ import './style.css'
 // import './bases/01-const-let'
 // import './bases/02-template-string'
 // import './bases/03-object-literal'
-// import './bases/04-arrays'
+import './bases/04-arrays'
 // import './bases/05-function'
 // import './bases/06-obj-destructuring'
-import './bases/07-array-destructuring'
+// import './bases/07-array-destructuring'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
