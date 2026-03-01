1f1c5bde6b3279c81abfb2013780b1b1e1322dbe
kuboks <kuboks@users.noreply.github.com>
2026-03-01 04:06:00 +0000

    deploy: e599b30e90569b6fc58fcfc074d7c5f1cc7db045

---
+++ docs/reforzamiento/assets/index-tL0x2D-v.js
@@ -0,0 +1,5 @@
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const c="0bBaVkfD9tYT9NiuK3ZEHQKt9W9ZqRgD",a=o=>{const r=document.createElement("img");r.src=o,document.body.append(r)},l=async()=>{const o=await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${c}`),{data:r}=await o.json();return r.images.original.url};l().then(a);document.querySelector("#app").innerHTML=`
+  <div>
+    <h1>Hello world</h1>
+  </div>
+`;
